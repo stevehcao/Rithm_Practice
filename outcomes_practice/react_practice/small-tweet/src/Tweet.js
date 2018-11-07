46db438428fe22e78e28d7e822b8fb4ff62c4cf9
Steve Cao <steve.cao7@gmail.com>
2018-11-06 19:29:53 -0800

    making a small tweet component with react and styled components

---
+++ outcomes_practice/react_practice/small-tweet/src/Tweet.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// make a component
+
+/* can have "export default" here instead of below*/ class Tweet extends Component {
+  // can have state
+  // can have default props
+  // life cycle methods
+
+  render() {
+    return <div>{/* something */}</div>;
+  }
+}
+
+export default Tweet;
